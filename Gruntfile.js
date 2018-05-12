07123735f84ac5b71edb93717f71f2581aca473d
Carlos Gamboa Encinas <carlo-x-17@hotmail.com>
2018-05-12 18:10:01 -0400

    CI-001: Created tasks for copying htmls using templates.

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function(grunt) {
+
+    var configName = grunt.option('config') || 'config.json';
+    var data = grunt.option('data') || 'data.json';
+
+    var config = grunt.file.readJSON(configName);
+
+
+    grunt.registerTask('generateIndex', function() {
+    
+        grunt.file.copy('src/index.html', config.buildFolder + '/Index.html' , {
+            process: function (files) {
+                return grunt.template.process(files,
+                    {
+                        data: {                           
+                            appName: config.appName
+                        }
+                    }
+                );
+            }
+        });
+    });
+
+    grunt.registerTask('generatePage1', function() {
+    
+        grunt.file.copy('src/page1.html', config.buildFolder + '/' + config.pageOneName + '.html' , {
+            process: function (files) {
+                return grunt.template.process(files,
+                    {
+                        data: {                           
+                            appName: config.appName
+                        }
+                    }
+                );
+            }
+        });
+    });
+    grunt.registerTask('generatePage2', function() {
+    
+        grunt.file.copy('src/page2.html', config.buildFolder + '/' + config.pageTwoName + '.html' , {
+            process: function (files) {
+                return grunt.template.process(files,
+                    {
+                        data: {                           
+                            appName: config.appName
+                        }
+                    }
+                );
+            }
+        });
+
+    });
+
+    grunt.registerTask('build',['generateIndex','generatePage1','generatePage2']);
+
+}
